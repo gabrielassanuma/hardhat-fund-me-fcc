d24b2f41d02f3c15755ae04a03932e50caefa182
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-10-04 18:09:20 +0100

    Deploy setu up, working locally and on goerli testnet

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
 const { networkConfig, developmentChain } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -7,18 +8,26 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = network.config.chainId
 
     let ethUsdPriceFeedAddress
-    if (developmentChain.includes(network.name)) {
+    if (chainId == 31337) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
-        ethUsdPriceFeedAddress = networkConfig[chainId][ethUsdPriceFeed]
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
-
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args: args,
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (
+        !developmentChain.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
     log("_________________________")
 }
 
