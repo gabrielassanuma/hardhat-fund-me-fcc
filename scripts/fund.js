0df3c0859250b639391d0f0ca98a4c88c940199f
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-10-09 15:35:35 +0100

    added scripts to fund and withdraw functions

---
+++ scripts/fund.js
@@ -0,0 +1,20 @@
+// to be used fund smart contract quickly
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Funding contract...")
+    const transactionResponse = await fundMe.fund({
+        value: ethers.utils.parseEther("0.1"),
+    })
+    await transactionResponse.wait(1)
+    console.log("Funded")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
