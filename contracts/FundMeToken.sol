e9496332fa1247a8363c4d87897b7dc0e3580cde
Gabriel Assanuma <gabriel.assanuma@gmail.com>
2022-10-10 14:29:17 +0100

    create FundMeToken

---
+++ contracts/FundMeToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FundMeToken is ERC20 {
+    constructor(uint256 totalSupply) ERC20("FundMeToken", "FMT") {
+        _mint(msg.sender, totalSupply);
+    }
+}
